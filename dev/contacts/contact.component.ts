9f2c5c6734ba3a2b4e0d0f21904160805ecb2b4f
jelliotartz <jelliotartz@gmail.com>
2016-03-31 15:48:10 -0500

    refactor 2-way data binding

--- dev/contacts/contact.component.ts
+++ dev/contacts/contact.component.ts
@@ -4,13 +4,36 @@ import {Component} from "angular2/core";
 @Component({
 	selector: "contact",
 	template: `
-		<input [(ngModel)]="contact.firstName" type= "text" >
-	  <div>
-	  	phone number: {{ contact.phone }} <br>
-	    email: {{ contact.email }}
+	<div>
+		<div>
+			<label for="first-name">First Name</label>
+			<input [(ngModel)]="contact.firstName" type="text" id="first-name">
 		</div>
+		<div>
+			<label for="last-name">Last Name</label>
+			<input [(ngModel)]="contact.lastName" type="text" id="last-name">
+		</div>
+		<div>
+			<label for="phone">Phone</label>
+			<input [(ngModel)]="contact.phone" type="text" id="phone">
+		</div>
+		<div>
+			<label for="email">Email</label>
+			<input [(ngModel)]="contact.email" type="text" id="email">
+		</div>
+	</div>
 	`,
 	inputs: ["contact"]
+	styles: [`
+		label {
+			display: inline-block;
+			width: 140px;
+		}
+
+		input {
+			width: 250px;
+		}
+	`]
 })
 
 export class ContactComponent {
