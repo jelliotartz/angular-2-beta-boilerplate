fc659504914c150076a10d965bded165c9100eae
jelliotartz <jelliotartz@gmail.com>
2016-04-01 11:13:11 -0500

    add routing

--- dev/contacts/contact.component.ts
+++ dev/contacts/contact.component.ts
@@ -1,27 +1,28 @@
 import {Component} from "angular2/core";
-
+import {ContactService} from "./contact.service";
+import {Contact} from "./contact"
 
 @Component({
 	selector: "contact",
 	template: `
-	<div>
 		<div>
-			<label for="first-name">First Name</label>
-			<input [(ngModel)]="contact.firstName" type="text" id="first-name">
+			<div>
+				<label for="first-name">First Name</label>
+				<input [(ngModel)]="contact.firstName" type="text" id="first-name">
+			</div>
+			<div>
+				<label for="last-name">Last Name</label>
+				<input [(ngModel)]="contact.lastName" type="text" id="last-name">
+			</div>
+			<div>
+				<label for="phone">Phone</label>
+				<input [(ngModel)]="contact.phone" type="text" id="phone">
+			</div>
+			<div>
+				<label for="email">Email</label>
+				<input [(ngModel)]="contact.email" type="text" id="email">
+			</div>
 		</div>
-		<div>
-			<label for="last-name">Last Name</label>
-			<input [(ngModel)]="contact.lastName" type="text" id="last-name">
-		</div>
-		<div>
-			<label for="phone">Phone</label>
-			<input [(ngModel)]="contact.phone" type="text" id="phone">
-		</div>
-		<div>
-			<label for="email">Email</label>
-			<input [(ngModel)]="contact.email" type="text" id="email">
-		</div>
-	</div>
 	`,
 	inputs: ["contact"]
 	styles: [`
